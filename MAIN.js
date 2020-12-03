0677ad370fe9ece961a04eaf714f8a9fd2ae71dd
Deepak-Singh1234 <71645902+Deepak-Singh1234@users.noreply.github.com>
2020-12-03 15:56:07 +0530

    Add files via upload

---
+++ MAIN.js
@@ -0,0 +1,120 @@
+canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color = "blue";
+ ctx.beginPath();
+  ctx.strokeStyle = color;
+   ctx.lineWidth = 2;
+    ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+     ctx.stroke();
+     canvas.addEventListener("mousedown", my_mousedown);
+     function my_mousedown(e){
+        mouse_x = e.clientX - canvas.offsetLeft;
+         mouse_y = e.clientY - canvas.offsetTop;
+          console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+           circle(mouse_x , mouse_y);
+     }
+     function  circle(mouse_x , mouse_y){
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+         ctx.lineWidth = 2;
+          ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+           ctx.stroke();    
+     }
+
+
+
+     canvas = document.getElementById("myCanvas");
+     ctx=canvas.getContext("2d");
+     color = "black";
+      ctx.beginPath();
+       ctx.strokeStyle = color;
+        ctx.lineWidth = 2;
+         ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+          ctx.stroke();
+          canvas.addEventListener("mousedown", my_mousedown);
+          function my_mousedown(e){
+             mouse_x = e.clientX - canvas.offsetLeft;
+              mouse_y = e.clientY - canvas.offsetTop;
+               console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+                circle(mouse_x , mouse_y);
+          }
+          function  circle(mouse_x , mouse_y){
+             ctx.beginPath();
+             ctx.strokeStyle = color;
+              ctx.lineWidth = 2;
+               ctx.arc(mouse_x, mouse_y, 44 ,0 , 2*Math.PI);
+                ctx.stroke();    
+          }
+
+
+          canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color = "red";
+ ctx.beginPath();
+  ctx.strokeStyle = color;
+   ctx.lineWidth = 2;
+    ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+     ctx.stroke();
+     canvas.addEventListener("mousedown", my_mousedown);
+     function my_mousedown(e){
+        mouse_x = e.clientX - canvas.offsetLeft;
+         mouse_y = e.clientY - canvas.offsetTop;
+          console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+           circle(mouse_x , mouse_y);
+     }
+     function  circle(mouse_x , mouse_y){
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+         ctx.lineWidth = 2;
+          ctx.arc(mouse_x, mouse_y, 48 ,0 , 2*Math.PI);
+           ctx.stroke();    
+     }
+
+
+     canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color = "yellow";
+ ctx.beginPath();
+  ctx.strokeStyle = color;
+   ctx.lineWidth = 2;
+    ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+     ctx.stroke();
+     canvas.addEventListener("mousedown", my_mousedown);
+     function my_mousedown(e){
+        mouse_x = e.clientX - canvas.offsetLeft;
+         mouse_y = e.clientY - canvas.offsetTop;
+          console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+           circle(mouse_x , mouse_y);
+     }
+     function  circle(mouse_x , mouse_y){
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+         ctx.lineWidth = 2;
+          ctx.arc(mouse_x, mouse_y, 43 ,43 , 2*Math.PI);
+           ctx.stroke();    
+     }
+
+
+
+     canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color = "green";
+ ctx.beginPath();
+  ctx.strokeStyle = color;
+   ctx.lineWidth = 2;
+    ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+     ctx.stroke();
+     canvas.addEventListener("mousedown", my_mousedown);
+     function my_mousedown(e){
+        mouse_x = e.clientX - canvas.offsetLeft;
+         mouse_y = e.clientY - canvas.offsetTop;
+          console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+           circle(mouse_x , mouse_y);
+     }
+     function  circle(mouse_x , mouse_y){
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+         ctx.lineWidth = 2;
+          ctx.arc(mouse_x, mouse_y, 47 ,47 , 2*Math.PI);
+           ctx.stroke();    
+     }
